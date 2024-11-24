6becdea53685e41d3470dc3e36cb1b3b6d10c9b9
isiagi <isiagigeofrey0@gmail.com>
2024-11-24 18:05:33 +0300

    chore

--- job_applications/models.py
+++ job_applications/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class JobApplication(models.Model):
     job = models.ForeignKey('job_listings.Job', on_delete=models.CASCADE)
-    applicant = models.ForeignKey('applicant_profiles.ApplicantProfile', on_delete=models.CASCADE)
+    applicant = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
     date_applied = models.DateField(auto_now_add=True)
     application_status = models.CharField(max_length=20, default='applied')
 
--- job_applications/views.py
+++ job_applications/views.py
@@ -1,9 +1,16 @@
 from rest_framework import viewsets
 from .models import JobApplication
 from .serializers import JobApplicationSerializer
-
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated
 
 class JobApplicationViewSet(viewsets.ModelViewSet):
     queryset = JobApplication.objects.all()
     serializer_class = JobApplicationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(applicant=self.request.user)
+
+    def get_queryset(self):
+        # Optionally: filter to show only user's own applications
+        return JobApplication.objects.filter(applicant=self.request.user)

5f63689b2e62ee7866f20a468b8237e480e8a7d8
isiagi <isiagigeofrey0@gmail.com>
2024-12-06 15:17:10 +0300

    refactor

--- education/models.py
+++ education/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 class Education(models.Model):
     degree = models.CharField(max_length=100, null=True, blank=True)
-    major = models.CharField(max_length=100)
-    institution = models.CharField(max_length=100)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    major = models.CharField(max_length=100, null=True, blank=True)
+    institution = models.CharField(max_length=100, null=True, blank=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
     user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- employment_history/models.py
+++ employment_history/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class EmploymentHistory(models.Model):
-    job_title = models.CharField(max_length=100)
-    company_name = models.CharField(max_length=100)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    job_title = models.CharField(max_length=100, null=True, blank=True)
+    company_name = models.CharField(max_length=100, null=True, blank=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
     user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
